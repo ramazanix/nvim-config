1727f4b8d93553e3204e94b3c621f7e3e4ac9e7b
ramazanix <ramazanui2002@gmail.com>
2024-12-29 07:22:31 +0300

    Fixed

--- lua/ramazanix/plugins/lsp/mason.lua
+++ lua/ramazanix/plugins/lsp/mason.lua
@@ -8,15 +8,6 @@ return {
 		local mason = require("mason")
 		local mason_lspconfig = require("mason-lspconfig")
 		local mason_tool_installer = require("mason-tool-installer")
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		local mason = require("mason")
-		local mason_lspconfig = require("mason-lspconfig")
-		local mason_tool_installer = require("mason-tool-installer")
 
 		mason.setup({
 			ui = {
