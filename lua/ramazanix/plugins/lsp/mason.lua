92ad5e1f5a572d873112a76048d6df91db952a56
ramazanix <ramazanui2002@gmail.com>
2024-12-29 05:29:05 +0300

    Formatted files

--- lua/ramazanix/plugins/lsp/mason.lua
+++ lua/ramazanix/plugins/lsp/mason.lua
@@ -1,42 +1,44 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✔",
-          package_pending = "⍨",
-          package_uninstalled = "✘"
-        },
-      },
-    })
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✔",
+					package_pending = "⍨",
+					package_uninstalled = "✘",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "ts_ls",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "lua_ls",
-        "prismals",
-        "pyright",
-      },
-    })
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"ts_ls",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"lua_ls",
+				"prismals",
+				"pyright",
+			},
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier",
-        "black",
-        "stylua"
-      },
-    })
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"black",
+				"stylua",
+				"pylint",
+				"eslint_d",
+			},
+		})
+	end,
 }
